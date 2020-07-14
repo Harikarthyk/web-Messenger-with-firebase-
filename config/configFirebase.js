048ed51a78bca0e8d670d9ca04562525acac16d3
Hari_karthyk <54505967+Harikarthyk@users.noreply.github.com>
2020-07-14 12:03:59 +0530

    Update configFirebase.js

--- config/configFirebase.js
+++ config/configFirebase.js
@@ -1,14 +1,7 @@
 
 import firebase from 'firebase' ;
 const FirebaseConfig = firebase.initializeApp({
-    apiKey: "AIzaSyAfbER9Ob_c6BDCKLsXOgve9Q-p91KBcc0",
-    authDomain: "web-messager.firebaseapp.com",
-    databaseURL: "https://web-messager.firebaseio.com",
-    projectId: "web-messager",
-    storageBucket: "web-messager.appspot.com",
-    messagingSenderId: "287319364970",
-    appId: "1:287319364970:web:cc6aacf8b4d705f20e4113",
-    measurementId: "G-GFQCP9Y81M"
+//     YOUR FIREBASE CONFIG API 
 });
 
 const configFirebase = FirebaseConfig.firestore();
